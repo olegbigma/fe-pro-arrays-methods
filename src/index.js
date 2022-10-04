06e22dd88c07afb2e438d4e5ac4ca8ecec4d7822
Oleh <oleh.bihma@rubygarage.org>
2022-10-04 19:10:25 +0300

    [FC-10] | Object methods

--- src/index.js
+++ src/index.js
@@ -4,56 +4,94 @@ import { goods } from './goods.js';
  * @param {string} brand
  * @returns {*}
  */
-const brandFilter = (brand) => {};
+const brandFilter = (brand) => {
+  const phoneBrand = goods.filter((phone) => phone.brand === brand);
+  return phoneBrand;
+};
 
 /**
  * @param {string} color
  * @returns {*}
  */
-const colorFilter = (color) => {};
+const colorFilter = (color) => {
+  const phoneColor = goods.filter((phone) => phone.color === color);
+  return phoneColor;
+};
 
 /**
  * @param {string} model
  * @returns {*}
  */
-const modelFilter = (model) => {};
+const modelFilter = (model) => {
+  const phoneModel = goods.filter((phone) => phone.model === model);
+  return phoneModel;
+};
 
 /**
  * @param {number} memory
  * @returns {*}
  */
-const memoryFilter = (memory) => {};
+const memoryFilter = (memory) => {
+  const phoneMemory = goods.filter((phone) => phone.memory === memory);
+  return phoneMemory;
+};
 
 /**
  * @param {number} price
  * @returns {*}
  */
-const priceFilter = (price) => {};
+const priceFilter = (price) => {
+  const phonePrice = goods.filter((phone) => phone.price === price);
+  return phonePrice;
+};
 
 /**
  * @param {string} country
  * @returns {*}
  */
-const countryFilter = (country) => {};
+const countryFilter = (country) => {
+  const brandCountry = goods.filter((phone) => phone.country === country);
+  return brandCountry;
+};
 
 /**
  * @param {string} os
  * @returns {*}
  */
-const osFilter = (os) => {};
+const osFilter = (os) => {
+  const phoneOs = goods.filter((phone) => phone.os === os);
+  return phoneOs;
+};
 
 /**
  * @param {number} from
  * @param {number} to
  */
-const rangeFilter = (from, to) => {};
+const rangeFilter = (from, to) => {
+  const rangePrice = goods.filter((phone) => phone.price >= from && phone.price <= to);
+  return rangePrice;
+};
 
-const minPriceReducer = () => {};
+const minPriceReducer = () => {
+  const minPrice = goods.reduce((accumulator, phone) => {
+    return Math.min(accumulator, phone.price);
+  }, Infinity);
+  return minPrice;
+};
 
-const maxPriceReducer = () => {};
+const maxPriceReducer = () => {
+  const maxPrice = goods.reduce((accumulator, phone) => {
+    return Math.max(accumulator, phone.price);
+  }, 0);
+  return maxPrice;
+};
 
-const toMaxSorter = () => {};
-const toMinSorter = () => {};
+const toMaxSorter = () => {
+  return goods.sort((a, b) => b.price - a.price);
+};
+const toMinSorter = () => {
+  return goods.sort((a, b) => a.price - b.price);
+};
 
 export const filters = {
   brandFilter,
